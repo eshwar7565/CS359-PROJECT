92931f982b7efebc4491a81534434949abacfb8f
eshwar <eshwarrachakonda02@gmail.com>
2023-10-23 12:12:55 +0530

    Implemented reducers for friends section

--- src/redux/slices/app.js
+++ src/redux/slices/app.js
@@ -1,5 +1,5 @@
 import { createSlice } from "@reduxjs/toolkit";
-
+import axios from "../../utils/axios";
 
 
 
@@ -16,10 +16,15 @@ const initialState = {
     message: null,
   },
 
+  users:[],
+  friends : [] ,
+  friendRequests:[],
+
 }
 
 
 const slice = createSlice({
+
   name: "app",
   initialState,
 
@@ -42,7 +47,27 @@ const slice = createSlice({
       state.snackbar.open = false;
       state.snackbar.message = null;
     },
+
+    updateUsers(state,action)
+    {
+      state.users = action.payload.users ;
+
+    },
+    updateFriends(state,action)
+    {
+      state.users = action.payload.friends ;
+      
+    },
+    updateFriendRequests(state,action)
+    {
+      state.users = action.payload.request ;
+      
+    },
+    
+    
+
   }
+
 
 })
 
@@ -81,3 +106,95 @@ export function UpdateSidebarType(type) {
   };
 }
 
+export function FetchUsers() {
+  return async (dispatch, getState) => {
+    await axios
+      .get(
+        "/user/get-users",
+
+        {
+          headers: {
+            "Content-Type": "application/json",
+            Authorization: `Bearer ${getState().auth.token}`,
+          },
+        }
+      )
+      .then((response) => {
+        console.log(response);
+        dispatch(slice.actions.updateUsers({ users: response.data.data }));
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  };
+}
+
+export function FetchAllUsers() {
+  return async (dispatch, getState) => {
+    await axios
+      .get(
+        "/user/get-all-verified-users",
+
+        {
+          headers: {
+            "Content-Type": "application/json",
+            Authorization: `Bearer ${getState().auth.token}`,
+          },
+        }
+      )
+      .then((response) => {
+        console.log(response);
+        dispatch(slice.actions.updateAllUsers({ users: response.data.data }));
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  };
+}
+export function FetchFriends() {
+  return async (dispatch, getState) => {
+    await axios
+      .get(
+        "/user/get-friends",
+
+        {
+          headers: {
+            "Content-Type": "application/json",
+            Authorization: `Bearer ${getState().auth.token}`,
+          },
+        }
+      )
+      .then((response) => {
+        console.log(response);
+        dispatch(slice.actions.updateFriends({ friends: response.data.data }));
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  };
+}
+
+export function FetchFriendRequests() {
+  return async (dispatch, getState) => {
+    await axios
+      .get(
+        "/user/get-requests",
+
+        {
+          headers: {
+            "Content-Type": "application/json",
+            Authorization: `Bearer ${getState().auth.token}`,
+          },
+        }
+      )
+      .then((response) => {
+        console.log(response);
+        dispatch(
+          slice.actions.updateFriendRequests({ requests: response.data.data })
+        );
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  };
+}
