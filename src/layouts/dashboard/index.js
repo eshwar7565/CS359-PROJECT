53c0425d68ffce81ecf6f2832eb84279b012378e
eshwar <eshwarrachakonda02@gmail.com>
2023-10-23 12:40:59 +0530

    Socket setup

--- src/layouts/dashboard/index.js
+++ src/layouts/dashboard/index.js
@@ -2,10 +2,70 @@ import React from "react";
 import { Navigate,Outlet } from "react-router-dom";
 import { Stack } from "@mui/material";
 import  Sidebar  from "./Sidebar";
-import {  useSelector } from "react-redux";
+import {  connect, useDispatch, useSelector } from "react-redux";
+import { useEffect } from "react";
+import { connectSocket, socket } from "../../socket";
+import { showSnackbar } from "../../redux/slices/app";
 const DashboardLayout = () => {
 
   const { isLoggedIn } = useSelector((state) => state.auth);
+
+  const user_id = window.localStorage.getItem("user_id");
+
+  const dispatch = useDispatch();
+  useEffect(()=>
+  {
+    if(isLoggedIn )
+    {
+      window.onload = function () 
+      {
+        if(!window.location.hash)
+
+        {
+          window.location = window.location + '#loaded' ;
+          window.location.reload() ;
+        }
+      }
+
+      window.reload();
+
+      if(!socket)
+      {
+        connectSocket(user_id);
+      }
+
+      // new friend request 
+
+      socket.on("new_friend-request" , () => 
+      {
+        dispatch(showSnackbar({severity :"success" , message : data.message})) ;
+      })
+
+      //request accepted
+      socket.on("request_accepted" , () => 
+      {
+        dispatch(showSnackbar({severity :"success" , message : data.message})) ;
+      })
+
+      socket.on("request_sent" , () => 
+      {
+        dispatch(showSnackbar({severity :"success" , message : data.message})) ;
+      })
+
+
+    }
+
+    return () =>
+    {
+      socket.off("new_friend_request");
+      socket.off("request_accepted");
+      socket.off("request_sent");
+
+    }
+
+  },[isLoggedIn,socket]) ;
+
+
   if (!isLoggedIn) {
     return <Navigate to={"/auth/login"} />;
   }
--- src/redux/slices/auth.js
+++ src/redux/slices/auth.js
@@ -4,35 +4,35 @@ import axios from "../../utils/axios";
 import { showSnackbar } from "./app";
 
 const initialState = {
-    isLoggedIn: false,
-    token: "",
-    isLoading: false,
-    // user: null,
-    // user_id: null,
-    // email: "",
-    // error: false,
-  };
+  isLoggedIn: false,
+  token: "",
+  isLoading: false,
+  // user: null,
+  // user_id: null,
+  // email: "",
+  // error: false,
+};
 
-  const slice = createSlice({
-    name: "auth",
-    initialState,
-    reducers: {
-        updateIsLoading(state, action) {
-            state.error = action.payload.error;
-            state.isLoading = action.payload.isLoading;
-          },
-        logIn(state, action) {
-            state.isLoggedIn = action.payload.isLoggedIn;
-            state.token = action.payload.token;
-            // state.user_id = action.payload.user_id;
-          },
-          signOut(state, action) {
-            state.isLoggedIn = false;
-            state.token = "";
-            state.isLoading=false;
-            // state.user_id = null;
-          },
-    }
+const slice = createSlice({
+  name: "auth",
+  initialState,
+  reducers: {
+    updateIsLoading(state, action) {
+      state.error = action.payload.error;
+      state.isLoading = action.payload.isLoading;
+    },
+    logIn(state, action) {
+      state.isLoggedIn = action.payload.isLoggedIn;
+      state.token = action.payload.token;
+      // state.user_id = action.payload.user_id;
+    },
+    signOut(state, action) {
+      state.isLoggedIn = false;
+      state.token = "";
+      state.isLoading = false;
+      // state.user_id = null;
+    },
+  }
 
 }
 );
@@ -80,52 +80,50 @@ export function ForgotPassword(formValues) {
 }
 
 export function LoginUser(formValues) {
-    return async (dispatch, getState) => {
-      // Make API call here
-  
-      dispatch(slice.actions.updateIsLoading({ isLoading: true, error: false }));
-  
-      await axios
-        .post(
-          "/auth/login",
-          {
-            ...formValues,
-          },
-          {
-            headers: {
-              "Content-Type": "application/json",
-            },
-          }
-        )
-        .then(function (response) 
-        {
-            console.log(response);
-            dispatch(
-              slice.actions.logIn({
-                isLoggedIn: true,
-                token: response.data.token,
-                user_id: response.data.user_id,
-              })
-            );
-            // window.localStorage.setItem("user_id", response.data.user_id);
-            dispatch(
-              showSnackbar({ severity: "success", message: response.data.message })
-            );
-            // dispatch(
-            //   slice.actions.updateIsLoading({ isLoading: false, error: false })
-            // );
-        })
-        .catch(function (error)
-        {
-          console.log(error);
-          dispatch(showSnackbar({ severity: "error", message: error.message }));
-        });
-    };
-  }
+  return async (dispatch, getState) => {
+    // Make API call here
 
-  export function LogoutUser() {
-    return async (dispatch, getState) => {
-      // window.localStorage.removeItem("user_id");
-      dispatch(slice.actions.signOut());
-    };
-  }
+    dispatch(slice.actions.updateIsLoading({ isLoading: true, error: false }));
+
+    await axios
+      .post(
+        "/auth/login",
+        {
+          ...formValues,
+        },
+        {
+          headers: {
+            "Content-Type": "application/json",
+          },
+        }
+      )
+      .then(function (response) {
+        console.log(response);
+        dispatch(
+          slice.actions.logIn({
+            isLoggedIn: true,
+            token: response.data.token,
+            user_id: response.data.user_id,
+          })
+        );
+        window.localStorage.setItem("user_id", response.data.user_id);
+        dispatch(
+          showSnackbar({ severity: "success", message: response.data.message })
+        );
+        // dispatch(
+        //   slice.actions.updateIsLoading({ isLoading: false, error: false })
+        // );
+      })
+      .catch(function (error) {
+        console.log(error);
+        dispatch(showSnackbar({ severity: "error", message: error.message }));
+      });
+  };
+}
+
+export function LogoutUser() {
+  return async (dispatch, getState) => {
+    window.localStorage.removeItem("user_id");
+    dispatch(slice.actions.signOut());
+  };
+}
