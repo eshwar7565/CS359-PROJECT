2dbd8a68f118f9522d1f6942b09c93663a491cb4
eshwar <eshwarrachakonda02@gmail.com>
2023-11-18 22:50:23 +0530

    Some changes

--- src/layouts/dashboard/index.js
+++ src/layouts/dashboard/index.js
@@ -1,23 +1,37 @@
 import React from "react";
 import { Navigate,Outlet } from "react-router-dom";
 import { Stack } from "@mui/material";
+
+// TODO
 import  Sidebar  from "./Sidebar";
 import {   useDispatch, useSelector } from "react-redux";
 import { useEffect } from "react";
 import { connectSocket, socket } from "../../socket";
 import { SelectConversation, showSnackbar } from "../../redux/slices/app";
-import { AddDirectConversation, UpdateDirectConversation } from "../../redux/slices/conversation";
+import {AddDirectMessage, AddDirectConversation, UpdateDirectConversation } from "../../redux/slices/conversation";
+
+
+
+// TODO audio and video calls 
 
 const DashboardLayout = () => {
 
+  const dispatch = useDispatch();
   const { isLoggedIn } = useSelector((state) => state.auth);
+  const {user_id} = useSelector((state) => state.auth);
 
-  const user_id = window.localStorage.getItem("user_id");
+
+  // TO DO audio and video calls 
+ 
 
   const { conversations, current_conversation } = useSelector(
     (state) => state.conversation.direct_chat
   );
-  const dispatch = useDispatch();
+  
+
+  // Use effect and get fetch user profile need to done
+
+
   useEffect(()=>
   {
     if(isLoggedIn )
@@ -25,20 +39,34 @@ const DashboardLayout = () => {
       window.onload = function () 
       {
         if(!window.location.hash)
-
         {
           window.location = window.location + '#loaded' ;
           window.location.reload() ;
         }
       }
-
-     
-
+      window.onload();
       if(!socket)
       {
         connectSocket(user_id);
       }
 
+      socket.on("new_message", (data) => {
+        const message = data.message;
+        console.log(current_conversation, data);
+        // check if msg we got is from currently selected conversation
+        if (current_conversation?.id === data.conversation_id) {
+          dispatch(
+            AddDirectMessage({
+              id: message._id,
+              type: "msg",
+              subtype: message.type,
+              message: message.text,
+              incoming: message.to === user_id,
+              outgoing: message.from === user_id,
+            })
+          );
+        }
+      });
       socket.on("start_chat", (data) => 
         {
           console.log(data);
@@ -72,9 +100,7 @@ const DashboardLayout = () => {
       socket.on("request_sent" , (data) => 
       {
         dispatch(showSnackbar({severity :"success" , message : data.message})) ;
-      })
-
-
+      });
     }
 
     return () =>
@@ -83,9 +109,10 @@ const DashboardLayout = () => {
       socket.off("request_accepted");
       socket.off("request_sent");
       socket?.off("start_chat");
-    }
+      socket?.off("new_message");
+    };
 
-  },) ;
+  }, [conversations,current_conversation,dispatch,user_id,isLoggedIn]) ;
 
 
   if (!isLoggedIn) {
