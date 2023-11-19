70359b051648b90a65cd4d61352ab5b9d0ee4269
eshwar <eshwarrachakonda02@gmail.com>
2023-11-19 22:44:22 +0530

    avatar update pending

--- src/pages/dashboard/Profile.js
+++ src/pages/dashboard/Profile.js
@@ -1,6 +1,8 @@
 import React, { useEffect } from "react";
-import { Box, IconButton, Stack, Typography } from "@mui/material";
-import { CaretLeft } from "phosphor-react";
+import { Box, 
+  // IconButton, 
+  Stack, Typography } from "@mui/material";
+// import { CaretLeft } from "phosphor-react";
 import ProfileForm from "../../sections/settings/settings/ProfileForm";
 import { useDispatch } from "react-redux";
 import { FetchUserProfile } from "../../redux/slices/app";
