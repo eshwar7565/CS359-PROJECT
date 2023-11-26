bf39c30dd5f7a99fd55682e1763ff3245cbdb5ad
eshwar <eshwarrachakonda02@gmail.com>
2023-11-26 19:50:07 +0530

    information as per side bar settings

--- src/pages/dashboard/GeneralApp.js
+++ src/pages/dashboard/GeneralApp.js
@@ -1,10 +1,12 @@
 import React from "react";
+import { useTheme } from "@mui/material/styles"
 import { Stack, Box,Typography } from "@mui/material";
 
 // import { Link } from "react-router-dom";
 import NoChat from "../../assets/Illustration/NoChat";
 
-import { useTheme } from "@mui/material/styles"
+// import ChatComponent from "./Conversation";
+
 import Chats from './Chats.js';
 import Conversation from "../../components/Conversation/index.js";
 import Contact from "../../components/Contact.js";
@@ -12,10 +14,8 @@ import { useSelector } from "react-redux";
 import SharedMessages from "../../components/SharedMessages.js";
 import StarredMessages from "../../components/StarredMessages.js";
 const GeneralApp = () => {
-
   const theme = useTheme();
   const { sideBar , room_id ,chat_type } = useSelector((state) => state.app);
-
   // const {current_conversation}=useSelector((state)=>state.conversation.direct_chat);
   return (
     <Stack direction="row" sx={{ width: "100%" }}>
@@ -32,7 +32,6 @@ const GeneralApp = () => {
           backgroundColor: theme.palette.mode === "light"
             ? "#F0F4FA"
             : theme.palette.background.default,
-
         }}
       >
         {
@@ -46,39 +45,27 @@ const GeneralApp = () => {
         alignItems="center"
         justifyContent={"center"}
         >
-
         <NoChat />
               <Typography variant="subtitle2">
                 Select a conversation or start a new one
-               
               </Typography>
         </Stack>
-
       }
-
       </Box>
-
       {sideBar.open &&
         (() => {
           switch (sideBar.type) {
             case "CONTACT":
               return <Contact />;
-
             case "STARRED":
               return <StarredMessages/>;
-
             case "SHARED":
               return <SharedMessages />;
-
             default:
               break;
           }
         })()}
-  
-
     </Stack>
-
   );
 };
-
 export default GeneralApp;
